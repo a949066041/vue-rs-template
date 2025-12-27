c55bc8d0fb980db682e4fe78cdd4607a61389b46
a949066041 <wangsh0508@gmail.com>
2025-12-27 22:44:57 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
-// import mdx from 'vite-plugin-mdx'
 import { version } from './package.json' with { type: 'json' }
-import { pluginDemoMdOptions } from './scripts/remark'
 
 const APP_TITLE = 'template vue vite'
 
@@ -11,7 +9,6 @@ export default defineConfig({
   plugins: [
     vue(),
     VueRouter(),
-    // mdx(pluginDemoMdOptions),
   ],
   define: {
     APP_TITLE: JSON.stringify(APP_TITLE),
