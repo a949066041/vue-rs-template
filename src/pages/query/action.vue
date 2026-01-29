22b1c1059961fb66e03000b1411a066520f1ed5f
a949066041 <949066041@qq.com>
2026-01-29 08:49:37 +0800

    chore: update dep

---
+++ src/pages/query/action.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useMutation } from '@pinia/colada'
+import { fetchUser } from '~/api'
+
+const { mutateAsync, data } = useMutation({
+  key: ['user-action'],
+  mutation: fetchUser,
+})
+</script>
+
+<template>
+  <div>
+    {{ data }}
+    <button @click="mutateAsync(1)">
+      action22
+    </button>
+  </div>
+</template>
--- src/setup/index.ts
+++ src/setup/index.ts
@@ -1,37 +1,9 @@
-import { watchEffect } from 'vue'
-import { useAuthStore, useCacheStore, useCountStore } from '~/store'
 import { setupFetch } from './fetch.setup'
 
 export * from './auth.setup'
 
 function setupApp() {
   setupFetch()
-  import.meta.env.DEV && addCustomeDevtoolsTab()
 }
 
 setupApp()
-
-function addCustomeDevtoolsTab() {
-  const countStore = useCountStore()
-  const authStore = useAuthStore()
-  const cacheStore = useCacheStore()
-
-  watchEffect(() => {
-    Object.entries(countStore).forEach(([key, value]) => {
-      if (typeof value !== 'function') {
-        console.warn('count', key, value.value)
-      }
-    })
-    Object.entries(authStore).forEach(([key, value]) => {
-      if (typeof value !== 'function') {
-        console.warn('auth', key, value.value)
-      }
-    })
-
-    Object.entries(cacheStore).forEach(([key, value]) => {
-      if (typeof value !== 'function') {
-        console.warn('cache', key, value.value)
-      }
-    })
-  })
-}
