73028faad0ddf94ad1e3fe831c9af69d64eafbed
a949066041 <949066041@qq.com>
2026-05-07 11:43:06 +0800

    chore(deps): update dependencies in package.json and pnpm-lock.yaml
    - Upgraded several dependencies to their latest versions, including @iconify/json, @pinia/colada, @rsbuild/core, and others.
    - Updated the TypeScript definitions for .vue and .mdx files in env.d.ts.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="@rsbuild/core/types" />
 /// <reference types="unplugin-vue-router/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<object, object, unknown>
+  export default component
+}
+
+declare module '*.mdx' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<object, object, unknown>
+  export default component
+}
